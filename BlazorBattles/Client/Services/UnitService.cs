a0e43999838b38b9af5153ff21ac5e6ab5386de6
Ben Haras-gummer <ben.haras-gummer@ge.com>
2021-08-05 13:21:30 +0100

    Add project files.

---
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -0,0 +1,45 @@
+﻿using BlazorBattles.Shared;
+using Blazored.Toast.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class UnitService : IUnitService
+    {
+        private readonly IToastService _toastService;
+        private readonly HttpClient _http;
+
+        public UnitService(IToastService toastService, HttpClient http)
+        {
+            _toastService = toastService;
+            _http = http;
+        }
+
+        public IList<Unit> Units { get; set; } = new List<Unit>();
+        public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
+
+
+        public void AddUnit(int unitId)
+        {
+            var unit = Units.First(unit => unit.Id == unitId);
+            MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
+            _toastService.ShowSuccess($"Your {unit.Title} has been built!", "Unit built");
+            /*Console.WriteLine($"{unit.Title} was built!");
+            Console.WriteLine($"Your army size: {MyUnits.Count}");*/
+        }
+
+        public async Task LoadUnitsASync()
+        {
+            if(Units.Count == 0)
+            {
+                Units = await _http.GetFromJsonAsync<IList<Unit>>("api/Unit");
+
+            }
+        }
+    }
+}
